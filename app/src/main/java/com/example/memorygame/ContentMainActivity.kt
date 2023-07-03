44adaa24623debcadcbada243fab870fa6aeb777
alupo <adrianlupoae2@gmail.com>
2023-07-03 13:20:50 +0300

    ia

---
+++ app/src/main/java/com/example/memorygame/ContentMainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.memorygame
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ContentMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_main)
+
+        // Alte acțiuni sau inițializări specifice
+    }
+}
--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -1,18 +1,33 @@
 
 package com.example.memorygame
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.example.memorygame.R.drawable.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var button: Button
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+         button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            //codul pentru deschiderea jocului
+            val intent = Intent(this@MainActivity, ContentMainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
 /*
         setSupportActionBar(toolbar)
 
