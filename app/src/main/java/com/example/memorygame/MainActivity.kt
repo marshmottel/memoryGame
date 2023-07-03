15431a8a3ffa0edc20ad527924eed20f65370c80
alupo <adrianlupoae2@gmail.com>
2023-07-03 16:53:57 +0300

    merge

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -34,57 +34,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
-
-       // setSupportActionBar(toolbar)
-/*
-        val images: MutableList<Int> =
-            mutableListOf(damacaro, nouacupa, patrutrefla, popatrefla, valetcaro, zecepica, damacaro, nouacupa, patrutrefla, popatrefla, valetcaro, zecepica)
-
-        val buttons = arrayOf(button1, button2, button3, button4, button5, button6, button7, button8,
-            button9, button10, button11, button12, )
-
-        val cardBack = cardback
-        var clicked = 0
-        var turnOver = false
-        var lastClicked = -1
-        var allCardsTurned = false
-
-        images.shuffle()
-        for(i in 0..11){
-            buttons[i].setBackgroundResource(cardBack)
-            buttons[i].text = "cardBack"
-            buttons[i].textSize = 0.0F
-            buttons[i].setOnClickListener {
-                if (buttons[i].text == "cardBack" && !turnOver) {
-                    buttons[i].setBackgroundResource(images[i])
-                    buttons[i].setText(images[i])
-                    if (clicked == 0) {
-                        lastClicked = i
-                    }
-                    clicked++
-                } else if (buttons[i].text !in "cardBack") {
-                    buttons[i].setBackgroundResource(cardBack)
-                    buttons[i].text = "cardBack"
-                    clicked--
-                }
-
-                if (clicked == 2) {
-                    turnOver = true
-                    if (buttons[i].text == buttons[lastClicked].text) {
-                        buttons[i].isClickable = false
-                        buttons[lastClicked].isClickable = false
-                        turnOver = false
-                        clicked = 0
-                    }
-                } else if (clicked == 0) {
-                    turnOver = false
-                }
-            }
-        }
-
-            */
-
     }
 
 }
