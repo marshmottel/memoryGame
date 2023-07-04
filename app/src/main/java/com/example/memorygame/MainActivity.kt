c61b5d4a887ddb565fe7fcdccf2a43b20a57fe95
alupo <adrianlupoae2@gmail.com>
2023-07-04 13:20:48 +0300

    Modification for the menu bar, initially we will have the menu button, then we can choose the type of game we want to launch.

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -1,75 +1,111 @@
-
 package com.example.memorygame
 
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuInflater
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.PopupMenu
 import com.example.memorygame.R.drawable.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import android.widget.PopupMenu
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
-    private lateinit var button13:Button
-    private lateinit var button14:Button
+    private lateinit var cardsButton: Button
+    private lateinit var buttonyu: Button
+    private lateinit var menuButton: Button
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        cardsButton = findViewById<Button>(R.id.cardsButton)
+        cardsButton.setOnClickListener {
 
-        val menuButton = findViewById<Button>(R.id.button14)
-        menuButton.setOnClickListener { v ->
-            val popupMenu = PopupMenu(this@MainActivity, v)
-            val inflater: MenuInflater = popupMenu.menuInflater
-            inflater.inflate(R.menu.menu_main, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.button -> {
-                        val page2 = Intent(this@MainActivity, ContentMainActivity::class.java)
-                        startActivity(page2)
+            val intent = Intent(this@MainActivity, ContentMainActivity::class.java)
+            startActivity(intent)
+
+        }
+        buttonyu = findViewById<Button>(R.id.buttonyu)
+        buttonyu.setOnClickListener {
+
+            val intent2 = Intent(this@MainActivity, ContentMainActivity2::class.java)
+            startActivity(intent2)
+
+        }
+
+
+
+        menuButton = findViewById<Button>(R.id.menuButton)
+        menuButton.setOnClickListener {
+            val popupMenu = PopupMenu(this@MainActivity, menuButton)
+            popupMenu.menuInflater.inflate(R.menu.menu_main, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.button00 -> {
+                        val intent00 =
+                            Intent(this@MainActivity, ContentMainActivity::class.java)
+                        startActivity(intent00)
                         true
                     }
-                    R.id.button13 -> {
-                        val page2 = Intent(this@MainActivity, ContentMainActivity2::class.java)
-                        startActivity(page2)
+
+                    R.id.button11 -> {
+                        val intent11 =
+                            Intent(this@MainActivity, ContentMainActivity2::class.java)
+                        startActivity(intent11)
                         true
                     }
+
                     else -> false
                 }
             }
+            popupMenu.show()
         }
-
-
-    }
-/*
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            R.id.button -> {
-                val intentPagina1 = Intent(this@MainActivity, ContentMainActivity::class.java)
-                startActivity(intentPagina1)
-                return true
-            }
-
-            R.id.button13 -> {
-                val intentPagina2 = Intent(this@MainActivity, ContentMainActivity2::class.java)
-                startActivity(intentPagina2)
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
 
-*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menuButton -> {
+                val popupMenu = PopupMenu(this@MainActivity, menuButton)
+                popupMenu.menuInflater.inflate(R.menu.menu_main, popupMenu.menu)
+
+                popupMenu.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.button00 -> {
+                            val intent00 =
+                                Intent(this@MainActivity, ContentMainActivity::class.java)
+                            startActivity(intent00)
+                            true
+                        }
+
+                        R.id.button11 -> {
+                            val intent11 =
+                                Intent(this@MainActivity, ContentMainActivity2::class.java)
+                            startActivity(intent11)
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
+                popupMenu.show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
 
 
