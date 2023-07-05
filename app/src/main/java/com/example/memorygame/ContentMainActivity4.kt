f7d17fa8de282834292f737ef0cdcbb710630f70
alupo <adrianlupoae2@gmail.com>
2023-07-05 14:46:37 +0300

    Commit for the most difficult level of the game

---
+++ app/src/main/java/com/example/memorygame/ContentMainActivity4.kt
@@ -0,0 +1,30 @@
+package com.example.memorygame
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_main.*
+import com.example.memorygame.R.drawable.*
+class ContentMainActivity4 : AppCompatActivity() {
+    private lateinit var back: Button
+    private lateinit var lastClickedButton: Button
+    private lateinit var lastClickedImage: String
+    private var matchedPairs = 0
+    private var mediaPlayer: MediaPlayer? = null
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_mediu)
+
+        back = findViewById<Button>(R.id.back)
+        back.setOnClickListener {
+
+            val intent2 = Intent(this@ContentMainActivity4, MainActivity::class.java)
+            startActivity(intent2)
+
+        }}}
