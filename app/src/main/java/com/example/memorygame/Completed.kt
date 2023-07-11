77b900e332c6b2360435e9f83a4511d4234da105
Marshmottel <ob133@student.ugal.ro>
2023-07-11 23:19:12 +0300

    am creat pagina de game completed si functioneaza

--- app/src/main/java/com/example/memorygame/Completed.kt
+++ app/src/main/java/com/example/memorygame/Completed.kt
@@ -1,13 +1,27 @@
 package com.example.memorygame
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.example.memorygame.R
+import kotlinx.android.synthetic.main.content_main.back
+
 
 class Completed : AppCompatActivity() {
+    private lateinit var back: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.completed)
+
+        back = findViewById<Button>(R.id.back)
+        back.setOnClickListener {
+
+            val intent2 = Intent(this@Completed, MainActivity::class.java)
+            startActivity(intent2)
+
+        }
 
     }
 }
